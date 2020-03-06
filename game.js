594dea11848626ce684b68c16f55b0f7069cd7cc
Tsel Anton <antontsel@yandex.ru>
2020-03-06 09:16:04 +0400

    Create carcass

--- game.js
+++ game.js
@@ -1,7 +1,39 @@
-var obj = {}
-var num = 1;
-var array = true;
+console.log("Start script...");
 
-console.log(typeof(obj));
-console.log(typeof(num));
-console.log(typeof(array));
+// Game const
+const shot = document.getElementById("shot");
+const record = document.getElementById("record");
+const hit = document.getElementById("hit");
+const dead = document.getElementById("dead");
+const table = document.getElementById("enemy");
+const buttonAgain = document.getElementById("again");
+
+// Game class
+const game = {
+	record: 0,
+	shot: 0,
+	hit: 0,
+	dead: 0
+};
+
+const state = {
+	hit(element) {
+		this.changeState(element, 'hit')
+	},
+	changeState(element, value) {
+		element.className = value;
+	}
+}
+
+// Get fire
+const fire = (event) => {
+	console.log(event.target);
+	state.hit(event.target);
+};
+
+// Game initialization
+const init = () => {
+	table.addEventListener('click', fire);
+};
+
+init();
